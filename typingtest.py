d00b2aa9b10284428b8753480fd4883510edb63b
wariomale <squirrelnuts987@gmail.com>
2021-12-30 10:52:51 +0100

    Implemented spell checking and WPM, although with major bugs.

--- typingtest.py
+++ typingtest.py
@@ -2,7 +2,12 @@
 
 import time
 import random
-from getkey import getkey
+import string
+import sys
+import string
+import sys
+
+#from getkey import getkey
 
 def generateTest():
     mylines = []                              
@@ -21,7 +26,35 @@ def speedCalc():
     end = time.time()
     timePassed = (end - start) 
     generateTestLen = len(generateTest())
-    return ((generateTestLen/5)/timePassed)*60
 
+    return generateTest(), timePassed, ((generateTestLen/5)/timePassed)*60
 
-print(speedCalc())
+def spellCheck():
+    test, timePassed, wpm = speedCalc()
+    diff = 0
+    correctChars = 0
+
+    file_A = generateTest()
+    file_B = test
+
+    #read_A=open(file_A,'r').read()
+    #read_B=open(file_B,'r').read()
+
+    for char_a, char_b in zip(file_A, file_B):
+        if char_a == char_b:
+            correctChars = correctChars+1
+    file_A_len = len(file_A)
+
+    correctPercent = (correctChars/file_A_len)*100
+
+    errors = file_A_len - correctChars
+    errorRate = errors/timePassed
+
+    netWPM = wpm - errorRate
+
+    return correctPercent, errors, netWPM
+
+correctPercent, errors, netWPM = spellCheck()
+print(correctPercent)
+print(errors)
+print(netWPM)
